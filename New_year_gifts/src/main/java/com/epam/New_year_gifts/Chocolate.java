0ef81a0b5b7cba0a1409ca7d9729e45c08ecfedc
Manasi <manasi.s224@gmail.com>
2020-02-08 17:48:33 +0530

    first

---
+++ New_year_gifts/src/main/java/com/epam/New_year_gifts/Chocolate.java
@@ -0,0 +1,21 @@
+package com.epam.New_year_gifts;
+
+public class Chocolate {
+	private int price, weight;
+	String name;
+	Chocolate(){
+		price=0;
+		weight=0;
+		name="";
+	}
+	Chocolate(int weight, int price){
+		this.weight=weight;
+		this.price=price;
+	}
+	int getPrice() {
+		return price;
+	}
+	int getWeight() {
+		return weight;
+	}
+}
